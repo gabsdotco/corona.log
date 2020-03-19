410384352f5dd3a3c32ec19acc9b8fab181c9e6a
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2020-03-19 01:43:18 -0300

    (feat) created the view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,27 @@
 <template>
-  <div class="home">
-    <div class="welcome-title">
-      <h1 id="strong-title">Hello,</h1>
-      <h1 id="light-title">let's start to see the statistics</h1>
+  <div class="container">
+    <div class="welcome">
+      <h1 class="welcome-text--strong">Hello,</h1>
+      <p class="welcome-text--light">find a region to see the statistics</p>
+    </div>
+    <div class="search">
+      <input type="text" placeholder="Insert the region" class="search-input" v-model="search">
+    </div>
+    <div class="filter" v-if="search">
+      <template v-if="hasResults">
+        <div
+          class="filter-card"
+          v-for="(country, index) in filter"
+          :key="index"
+          @click="onClick(country)">
+          {{ country }}
+        </div>
+      </template>
+      <template v-else>
+        <div class="filter-empty-state">
+          <p>No results</p>
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -25,9 +44,13 @@ export default {
     filter() {
       const filter = this.countries?.filter((country) => country
         .toLowerCase()
-        .includes(this.search?.toLowerCase()));
+        .includes(this.search.toLowerCase()));
 
       return filter?.splice(0, 3);
+    },
+
+    hasResults() {
+      return this.filter.length > 0;
     },
   },
 
@@ -37,36 +60,110 @@ export default {
         .then((response) => response.json())
         .then((data) => Object.keys(data));
     },
+
+    onClick(country) {
+      this.$router.push({
+        path: `/log/${country.toLowerCase()}`,
+      });
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-$primary: rgb(255, 206, 70);
+@import '@/assets/variables.scss';
 
-.home {
-  margin: auto;
+@keyframes fade {
+  from {opacity: 0;}
+  to {opacity: 1;}
+}
+
+@keyframes filter {
+  from {height: 120px;}
+  to {height: 150px;}
+}
+
+.container {
   max-width: 600px;
-  padding: 50px;
   height: 100vh;
+  padding: 50px;
+  margin: auto;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  animation-name: fade;
+  animation-duration: 4s;
 
-  .welcome-title {
+  .welcome {
     width: 100%;
     line-height: 35px;
 
-    #strong-title {
+    .welcome-text--strong {
       font-size: 28px;
       font-weight: 700;
       color: $primary;
     }
 
-    #light-title {
+    .welcome-text--light {
       font-size: 26px;
       font-weight: 400;
+    }
+  }
+
+  .search {
+    width: 100%;
+
+    .search-input {
+      width: 100%;
+      margin: 40px 0px;
+      padding: 20px 0px;
+      font-size: 24px;
+      text-align: center;
+      outline: none;
+      border: none;
+      border-bottom: 1px solid #f5f5f5;
+      color: #d8d8d8;
+
+      &::placeholder {
+        color: #e4e4e4;
+      }
+    }
+  }
+
+  .filter {
+    width: 100%;
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    animation-name: filter;
+    animation-duration: 1.5s;
+
+    .filter-card {
+      padding: 15px;
+      font-size: 18px;
+      text-align: center;
+      color: #d8d8d8;
+      transition: 1s;
+      animation-name: fade;
+      animation-duration: 1.5s;
+      cursor: pointer;
+
+      &:hover {
+        color: $primary;
+      }
+    }
+
+    .filter-empty-state {
+      padding: 15px;
+      font-size: 18px;
+      text-align: center;
+      color: #d8d8d8;
+      transition: 1s;
+      animation-name: fade;
+      animation-duration: 1.5s;
     }
   }
 }
---
+++ src/views/Log.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="container">
+    <div class="stats">
+      <div class="stats-title">
+        <div class="stats-title--head">{{ capitalizedCountry }},</div>
+        <div class="stats-title--date">Log from {{ logCalendar }}, {{ logDate }}</div>
+      </div>
+      <div class="stats-wrapper" v-if="lastLog">
+        <div class="stats-wrapper-item">
+          {{ lastLog.confirmed }}
+          <span>Confirmed</span>
+        </div>
+        <div class="stats-wrapper-item">
+          {{ newCases }}
+          <span>New cases</span>
+        </div>
+        <div class="stats-wrapper-item">
+          {{ lastLog.deaths }}
+          <span>{{ lastLog.deaths > 1 ? 'Deaths' : 'Death' }}</span>
+        </div>
+        <div class="stats-wrapper-item">
+          {{ lastLog.recovered }}
+          <span>Recovered</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'Log',
+
+  props: {
+    country: String,
+  },
+
+  data: () => ({
+    logs: null,
+  }),
+
+  async created() {
+    await this.getLog();
+  },
+
+  computed: {
+    lastLog() {
+      return this.logs?.slice(-1)[0];
+    },
+
+    previousLog() {
+      return this.logs?.slice(-2)[0];
+    },
+
+    newCases() {
+      return this.lastLog?.confirmed - this.previousLog?.confirmed;
+    },
+
+    logDate() {
+      return moment(this.lastLog?.date).format('MM/DD/YYYY');
+    },
+
+    logCalendar() {
+      return moment(this.lastLog?.date).format('dddd');
+    },
+
+    capitalizedCountry() {
+      return this.country.replace(/\w\S*/g, (txt) => txt
+        .charAt(0)
+        .toUpperCase() + txt.substr(1)
+        .toLowerCase());
+    },
+  },
+
+  methods: {
+    async getLog() {
+      this.logs = await fetch('https://pomber.github.io/covid19/timeseries.json')
+        .then((response) => response.json())
+        .then((data) => data[this.capitalizedCountry]);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/variables.scss';
+
+@keyframes fade {
+  from {opacity: 0;}
+  to {opacity: 1;}
+}
+
+.container {
+  margin: auto;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation-name: fade;
+  animation-duration: 4s;
+
+  .stats {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .stats-title {
+      margin-bottom: 40px;
+
+      .stats-title--head {
+        color: $primary;
+        font-size: 26px;
+        font-weight: 700;
+      }
+
+      .stats-title--date {
+        color: rgb(78, 78, 78);
+      }
+    }
+
+    .stats-wrapper {
+      width: 100%;
+      max-width: 320px;
+      height: 250px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-wrap: wrap;
+
+      .stats-wrapper-item {
+        width: 50%;
+        height: 125px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-size: 24px;
+        font-weight: 700;
+        color: $primary;
+
+        &:first-child {
+          border-bottom: 0.5px solid $border;
+          border-right: 0.5px solid $border;
+        }
+
+        &:nth-child(2) {
+          border-bottom: 0.5px solid $border;
+          border-left: 0.5px solid $border;
+        }
+
+        &:nth-child(3) {
+          border-top: 0.5px solid $border;
+          border-right: 0.5px solid $border;
+        }
+
+        &:last-child {
+          border-top: 0.5px solid $border;
+          border-left: 0.5px solid $border;
+        }
+
+        span {
+          font-weight: 400;
+          font-size: 16px;
+          color: rgb(156, 156, 156);
+        }
+      }
+    }
+  }
+}
+</style>
