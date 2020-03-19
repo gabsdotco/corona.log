b04a0fbff7eb349dd1b308811a42ffe78cb8e0aa
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2020-03-19 01:43:42 -0300

    (feat) finished the first version

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
-    <router-view/>
+    <navbar />
+    <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,700,800,900&display=swap');
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Home from '../views/Home.vue';
+import Log from '@/views/Log.vue';
+import Home from '@/views/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,12 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/log/:country',
+    name: 'Log',
+    component: Log,
+    props: true,
   },
 ];
 
