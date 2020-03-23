c8ea157ee42165e158287c3140f1c0ba597a1df4
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2020-03-23 12:53:41 -0300

    (feat) added i18n

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+
 import App from './App.vue';
+import messages from './translation/messages';
 import router from './router';
+
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: 'pt_BR',
+  messages,
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
