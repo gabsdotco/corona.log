10e951087e98ac4cf12b490e801a24be926d0235
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2020-04-12 22:34:54 -0300

    (change) changed the structure

--- src/main.js
+++ src/main.js
@@ -1,19 +1,11 @@
 import Vue from 'vue';
-import VueI18n from 'vue-i18n';
 import VueMoment from 'vue-moment';
-
 import App from './App.vue';
-import messages from './translation/messages';
+
+import i18n from './utils/translation';
 import router from './router';
 
-Vue.use(VueI18n);
 Vue.use(VueMoment);
-
-const i18n = new VueI18n({
-  locale: 'pt_BR',
-  messages,
-});
-
 Vue.config.productionTip = false;
 
 new Vue({
