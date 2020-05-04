90d5edd24619bd774d2c9da088f20ce7193108d0
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2020-05-04 07:21:22 -0300

    (refactor) adjusts on the translation organization

--- src/utils/translation/messages.js
+++ src/utils/translation/messages.js
@@ -1,46 +1,7 @@
-module.exports = {
-  pt_BR: {
-    home: {
-      strong: 'Olá',
-      light: 'busque uma região para ver as estatísticas',
-      input: 'Insira uma região',
-      empty_state: 'Sem resultados',
-    },
-    log: {
-      date_description: 'Registro de',
-      stats: {
-        confirmed: 'Confirmados',
-        new_cases: 'Novos casos',
-        deaths: 'Mortos',
-        recovered: 'Recuperados',
-      },
-    },
-    components: {
-      empty_state: {
-        message: 'Sem registros para esse pais',
-      },
-    },
-  },
-  en: {
-    home: {
-      strong: 'Hello',
-      light: 'find a region to see the statistics',
-      input: 'Insert a region',
-      empty_state: 'No results',
-    },
-    log: {
-      date_description: 'Log from',
-      stats: {
-        confirmed: 'Confirmed',
-        new_cases: 'New cases',
-        deaths: 'Deaths',
-        recovered: 'Recovered',
-      },
-    },
-    components: {
-      empty_state: {
-        message: 'No logs for that country',
-      },
-    },
-  },
+import Portuguese from './languages/portuguese';
+import English from './languages/english';
+
+export default {
+  pt_BR: Portuguese,
+  en: English,
 };
